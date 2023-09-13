91b84e9bf9bb7bf656bfc83dad59ba73135edcfb
ApuiIrom <114992036+ApuiIrom@users.noreply.github.com>
2023-09-13 14:24:21 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (49.62%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -4,10 +4,9 @@ class Solution {
     //should return the length of the last word without space
     //string consist only letters and space
         String s1=s.trim();
-        char c1[]=s1.toCharArray();
         int count=0;
         for(int i=s1.length()-1;i>=0;i--){
-            if(c1[i]!=' ')
+            if(s1.charAt(i)!=' ')
                 count++;
             else
                 break;
