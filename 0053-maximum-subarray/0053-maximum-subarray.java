72f42b8f3011406d2c7294ea3afd737d0246680d
ApuiIrom <114992036+ApuiIrom@users.noreply.github.com>
2023-11-16 12:33:49 +0530

    Time: 22 ms (7.43%), Space: 56.4 MB (94.99%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int[] maxEnding=new int[nums.length];
+        maxEnding[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(maxEnding[i-1]+nums[i]>nums[i])
+                maxEnding[i]=maxEnding[i-1]+nums[i];
+            else
+                maxEnding[i]=nums[i];
+        }
+        Arrays.sort(maxEnding);
+        return maxEnding[nums.length-1];
+    }
+}
