2bb07fc8334556e7e482c4c4987531a7637ff70e
ApuiIrom <114992036+ApuiIrom@users.noreply.github.com>
2023-12-26 23:13:12 +0530

    Time: 2 ms (14.62%), Space: 44.5 MB (14.36%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 class Solution {
     public int removeDuplicates(int[] nums) {
-      int k=1;
-    	int tem[]=new int[nums.length];
-    	tem[0]=nums[0];
-    	for(int i=0;i<nums.length-1;i++) {
-    		if(nums[i]<nums[i+1]) {
-    			tem[k++]=nums[i+1];
-    		}
-    	}
-        for(int i=0;i<k;i++)
-            nums[i]=tem[i];
-    return k;
+     LinkedList<Integer> stack=new LinkedList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(stack.isEmpty())
+                stack.push(nums[i]);
+            else if(!stack.isEmpty() && stack.peek()!=nums[i])
+            {
+                stack.push(nums[i]);    
+                nums[stack.size()-1]=stack.peek();
+            }
+        }
+        return stack.size();
     }
 }
