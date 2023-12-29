81ef60972a75eae6d431a3d2064c9686cba7b736
ApuiIrom <114992036+ApuiIrom@users.noreply.github.com>
2023-12-29 18:34:04 +0530

    Time: 0 ms (100.00%), Space: 42 MB (6.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,21 +6,22 @@ class Solution {
      while(low<=high)
      {
       int mid=(low+(high-low)/2);
+         
       if(nums[mid]==target)
           return mid;
-      else if(nums[low]<=nums[mid])
+      else if(nums[low]<=nums[mid])//check whether first half of the array is sorted or not
       {
-         if(nums[low]<=target && target<nums[mid])
+         if(nums[low]<=target && target<nums[mid])//checking whether the target is in the first sorted half or not
              high=mid-1;
           else
-              low=mid+1;
+              low=mid+1;//low is set to mid+1 to shift to second unsorted since the target might present in there
       }
-        else
-        {   
-            if(nums[mid]<target && target<=nums[high])
+        else//if comes to else part means that the second half of the array is a sorted one
+        { 
+            if(nums[mid]<target && target<=nums[high])//checking whether the target is in the second sorted half or not
              low=mid+1;
             else
-              high=mid-1;
+              high=mid-1;//high is set to mid-1 since the target is not there in the second sorted half
         }
      } 
         return -1;
