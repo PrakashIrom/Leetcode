3bc0e9eb67988f11700c82fb61bbba679b63aba2
Prakash Irom <114992036+PrakashIrom@users.noreply.github.com>
2024-04-18 11:26:44 +0530

    Time: 300 ms (90.68%), Space: 51.3 MB (40.54%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.kt
+++ 0189-rotate-array/0189-rotate-array.kt
@@ -1,30 +1,25 @@
 class Solution {
     fun rotate(nums: IntArray, k: Int): Unit {
         
-        val len=nums.size
-        var aclen=len-1
-        val n = k%len
-        val result=Array(len){0}
-        var index=0
-        // reverse to find the starting element
-        // forward to start storing the element to the new array
-        //21 12 21 
-       // 12 4 21 12 21 12
+        val key=k%nums.size
         
-        for(i in 0 until n)
-        {
-            index=(aclen-i)%len
-        }
+        rotateArray(0,nums.size-1,nums)
+        rotateArray(0,key-1,nums)
+        rotateArray(key,nums.size-1,nums)
         
-        for(i in 0 until len)
-        {
-            result[i]=nums[(index+i)%len]
-        }
+    }
+    
+    fun rotateArray(low:Int,high:Int,arr:IntArray){
         
-        for(i in 0 until len)
-        {
-            nums[i]=result[i]
-        }
-     
+         var lowIndex = low
+         var highIndex = high
+        
+        while(lowIndex<highIndex){
+            val temp:Int=arr[lowIndex]
+            arr[lowIndex]=arr[highIndex]
+            arr[highIndex]=temp
+            lowIndex++
+            highIndex--
+        }   
     }
 }
