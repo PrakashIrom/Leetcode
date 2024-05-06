f6f284e98f4941d163afa4c757faf9abea8d8882
Prakash Irom <114992036+PrakashIrom@users.noreply.github.com>
2024-05-07 02:04:28 +0530

    Time: 201 ms (21.53%), Space: 41.7 MB (5.21%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.kt
@@ -0,0 +1,35 @@
+/**
+ * Example:
+ * var ti = Node(5)
+ * var v = ti.`val`
+ * Definition for a Node.
+ * class Node(var `val`: Int) {
+ *     var next: Node? = null
+ *     var random: Node? = null
+ * }
+ */
+
+class Solution {
+    fun copyRandomList(node: Node?): Node? {
+        
+        val map:HashMap<Node?, Node?> = HashMap<Node?,Node?>()
+        var curr=node
+        
+        while(curr!=null){
+            var copy=Node(curr!!.`val`)
+            map.put(curr,copy)
+            curr=curr?.next
+        }
+        curr=node
+        while(curr!=null){
+            (map[curr])?.next=map[curr?.next]
+            (map[curr])?.random=map[curr?.random]
+            curr=curr?.next
+        }
+        
+        curr=node
+        
+        val result: Node?=map[curr]
+      return result
+    }
+}
