cdd5c1c3282bb1a54d0c4e1ce2734506f095a793
Prakash Irom <114992036+PrakashIrom@users.noreply.github.com>
2024-05-20 12:26:59 +0530

    Time: 268 ms (35.18%), Space: 43.9 MB (45.37%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun setZeroes(matrix: Array<IntArray>): Unit {
+        var setRow = HashSet<Int>()
+        var setCol = HashSet<Int>()
+        
+        for(i in 0 until matrix.size){
+            for(j in 0 until matrix[0].size){
+                if(matrix[i][j]==0){
+                    setRow.add(i)
+                    setCol.add(j)
+                }
+            }
+        }
+        
+        for(i in 0 until matrix.size){
+            for(j in 0 until matrix[0].size){
+                if(setRow.contains(i) || setCol.contains(j)){
+                    matrix[i][j]=0
+                }
+            }
+        }
+    }
+}
