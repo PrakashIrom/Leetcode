7b3e6756e65fd2150da219ace3beb1985edf3d4c
Prakash Irom <114992036+PrakashIrom@users.noreply.github.com>
2024-09-25 14:16:42 +0530

    Time: 168 ms (69.43%), Space: 37.1 MB (54.89%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.kt
@@ -0,0 +1,22 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    
+    var height = 1
+    var max = 0
+    
+    fun maxDepth(root: TreeNode?): Int {
+        
+        if(root==null) return 0
+        
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1
+    }
+}
