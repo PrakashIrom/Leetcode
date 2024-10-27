9802c74a737a9b71f2c74daf3be218c44fb8320d
Prakash Irom <114992036+PrakashIrom@users.noreply.github.com>
2024-10-27 13:51:06 +0530

    Time: 160 ms (44.44%), Space: 34.3 MB (73.37%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.kt
@@ -1,27 +1,13 @@
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        // the first for loop should be iterated until the size of the minimum string length between the two
-        // the second while loop should be start from p1/p2 until the length of the maximum string length
-        var p:Int = 0
-        var result = ""
         
-        while(p< minOf(word1.length, word2.length)){
-            result+=word1[p]
-            result+=word2[p]
-            p++
+        var result = StringBuilder()
+        
+        for(i in 0 until maxOf(word1.length, word2.length)){
+            if(i<word1.length) result.append(word1[i])
+            if(i<word2.length) result.append(word2[i])
         }
         
-        if(word1.length>word2.length){
-            while(p<word1.length){
-                result+=word1[p++]
-            }
-        }
-        else if(word1.length<word2.length){
-            while(p<word2.length){
-                result+=word2[p++]
-            }
-        }
-        
-        return result
+        return result.toString()
     }
 }
