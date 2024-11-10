8f9ea26483facacd082b4ceb3725fcf6f5075aa3
Prakash Irom <114992036+PrakashIrom@users.noreply.github.com>
2024-11-10 11:37:39 +0530

    Time: 5 ms (74.60%), Space: 40.6 MB (31.63%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.kt
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.kt
@@ -1,20 +1,26 @@
 class Solution {
     fun longestOnes(nums: IntArray, k: Int): Int {
+        
         var low = 0
-        var count = 0
+        var zerosCount = 0
         var max = 0
+        
         // [1,1,1,0,0,0,0,1,1,1,1,1,1,1] k=3
         //move the high to the right and when it encounters 0s, increment the count until reaches k 
         // when count==k assign count=0 take max=maxOf(max,high-low+1)
-        
-        for(high in nums.indices){
-            if(nums[high]==0) count++
-            while(count>k){
-                if(nums[low]==0)count--
+        //right-low>k
+       
+        for(curr in nums.indices){
+            
+            if(nums[curr]==0) zerosCount++
+            
+            while(zerosCount>k){
+                if(nums[low]==0) zerosCount--
                 low++
             }
-            max = maxOf(high-low+1, max)
+            max = maxOf(max, curr-low+1)
         }
+        
         return max
     }
 }
