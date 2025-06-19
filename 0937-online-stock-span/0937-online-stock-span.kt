07c158c6e23becc9093b757d90f81293c0caceb1
Prakash Irom <114992036+PrakashIrom@users.noreply.github.com>
2025-06-19 22:16:44 +0530

    Time: 62 ms (93.81%), Space: 78.7 MB (61.95%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.kt
@@ -0,0 +1,43 @@
+import java.util.ArrayDeque
+
+class StockSpanner() {
+    
+    var stock=ArrayDeque<Int>()
+    var span=ArrayDeque<Int>()
+   
+    
+    fun next(price: Int): Int {
+        
+        var count=1
+        
+        /*if(stock.isEmpty())
+        {
+            stock.push(price)
+            span.push(1)
+        }
+        else
+        {*/
+            
+           while(stock.isNotEmpty() && stock.peek()<=price)
+            {
+                count+=span.pop()
+                stock.pop()
+            }
+              
+            span.push(count)
+            stock.push(price)
+            
+        //}
+        
+      return count
+    }
+                   
+}
+
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = StockSpanner()
+ * var param_1 = obj.next(price)
+ */
